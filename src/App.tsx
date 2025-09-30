9efc3dbeedf5f25288dad77e59fdd5a9dbefaf5b
Gabriel Araújo <71518003+gabrielaraujocomp@users.noreply.github.com>
2025-09-30 16:33:19 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -50,9 +50,11 @@ function App() {
       </p>
 
       <div className="bg-[#00c56e]/20 backdrop-blur-sm p-4 rounded-2xl inline-block">
-        <button className="bg-[#00c56e] text-white px-16 py-4 rounded-full text-lg font-semibold hover:bg-[#00b05f] transition-colors shadow-lg max-w-xs w-full mx-auto block">
+        
+<button className="bg-[#00c56e] text-white px-16 py-4 rounded-full text-lg font-semibold hover:bg-[#00b05f] transition-colors shadow-lg max-w-xs w-full mx-auto block">
   Garantir acesso!
 </button>
+        
       </div>
     </div>
   </div>
